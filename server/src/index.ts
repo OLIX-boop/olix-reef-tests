1fc20af02e49b88bd8e3efe359dcca4274561d92
OLIX <80951658+OLIX-boop@users.noreply.github.com>
2024-01-16 21:38:46 +0100

    edited some things

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import cors from 'cors';
 import DB from './database';
+import cors from 'cors';
 
 const app = express().use(cors());
-const port:number = 4000;
+const port:number = 1080;
 
 app.get('/', async (req, res) => {
     const result = await DB.fetchData();
+    console.log("GET REQUEST RECIVED - "+ new Date().toJSON());
     res.send(result);
 });
 
